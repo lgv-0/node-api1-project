2d6ad609395ba48538d72b0095993ae67b25eb63
Logan Van Hook <loganvan@att.net>
2020-03-09 15:16:23 -0400

    Match specifications

--- index.js
+++ index.js
@@ -17,10 +17,24 @@ server.get("/", (req, res)=>
 
 server.post("/api/users", (req, res)=>
 {
-    const UserData = req.body;
-    UserData.id = shortid();
+    if (req.body.name === undefined || req.body.bio === undefined)
+    {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+        return;
+    }
 
-    Users.push(UserData);
+    const UserData = req.body;
+
+    try
+    {
+        UserData.id = shortid();
+        Users.push(UserData);
+    }
+    catch
+    {
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" });
+        return;
+    }
 
     res.status(201).json(UserData);
 });
@@ -29,7 +43,17 @@ server.get("/api/users", (req, res)=>
 {
     if (req.query.id !== undefined)
     {
-        let Find = Users.filter((i)=>{return i.id === req.query.id})[0];
+        let Find;
+        
+        try
+        {
+            Users.filter((i)=>{return i.id === req.query.id})[0];
+        }
+        catch
+        {
+            res.status(500).json({ errorMessage: "The user information could not be retrieved." });
+            return;
+        }
 
         if (Find === undefined)
             res.status(404).json({error:"User not found"});
@@ -37,7 +61,16 @@ server.get("/api/users", (req, res)=>
             res.status(200).json(Find);
     }
     else
-        res.status(200).json(Users);
+    {
+        try
+        {
+            res.status(200).json(Users);
+        }
+        catch
+        {
+            res.status(500).json({ errorMessage: "The users information could not be retrieved." });
+        }
+    }
 });
 
 server.delete("/api/users", (req, res)=>
@@ -48,17 +81,24 @@ server.delete("/api/users", (req, res)=>
     {
         let Deleted = null;
 
-        Users = Users.filter((i)=>
+        try
         {
-            if (i.id !== req.query.id)
-                return true;
+            Users = Users.filter((i)=>
+            {
+                if (i.id !== req.query.id)
+                    return true;
 
-            Deleted = i;
-            return false;
-        });
+                Deleted = i;
+                return false;
+            });
+        }
+        catch
+        {
+            res.status(500).json({ errorMessage: "The user could not be removed" });
+        }
 
         if (Deleted === null)
-            res.status(404).json({error:"User not found"});
+            res.status(404).json({ message: "The user with the specified ID does not exist." });
         else
             res.status(200).json(Deleted);
     }
@@ -70,7 +110,23 @@ server.patch("/api/users", (req, res)=>
         res.status(400).json({error:"Invalid request paramaters"});
     else
     {
-        let FindUser = Users.filter((i)=>{return i.id === req.query.id})[0];
+        if (req.body.name === undefined || req.body.bio === undefined)
+        {
+            res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+            return;
+        }
+
+        let FindUser = undefined;
+        
+        try
+        {
+            FindUser = Users.filter((i)=>{return i.id === req.query.id})[0];
+        }
+        catch
+        {
+            res.status(500).json({ errorMessage: "The user information could not be modified." });
+            return;
+        }
         
         if (FindUser === undefined)
         {
